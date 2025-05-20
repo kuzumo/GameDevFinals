a812306144617e1b233b6603a9d901233f2da8b2
kuzumo <87818085+kuzumo@users.noreply.github.com>
2025-05-20 23:40:14 +0800

    update

--- GAMEDEV2_ACTIVITY cmp/Assets/MultipleChoiceDialog.cs
+++ GAMEDEV2_ACTIVITY cmp/Assets/MultipleChoiceDialog.cs
@@ -3,21 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 using XNode;
 
-public class MultipleChoiceDialog : BaseNode {
+public class MultipleChoiceDialog : BaseNode
+{
 
-	[Input] public string entry;
-	[Output] public string a;
-	[Output] public string b;
-[TextArea(7,20)]
-	public string dialogText;
-	public Sprite dialogImage;
-	
 
-	 public override string getDialogText(){
+    
+    [Input] public string entry;
+    [Output] public string a;
+    [Output] public string b;
+    [TextArea(7, 20)]
+    public string dialogText;
+    public Sprite dialogImage;
+
+
+    public override string getDialogText()
+    {
         return dialogText;
     }
 
-    public override Sprite getSprite(){
+    public override Sprite getSprite()
+    {
         return dialogImage;
     }
 }
